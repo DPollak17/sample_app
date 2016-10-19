e23c267b70b7cfef5b97921657760a0f2006050f
Derek Pollak <derek.pollak@gmail.com>
2016-10-19 12:20:17 -0400

    First commit

--- giphy_app.py
+++ giphy_app.py
@@ -7,17 +7,18 @@ app = Flask(__name__)
 #Defines function to pull the GIFs
 def get_gif(search_term):
     g = giphypop.Giphy()
-    results = g.search(search_term) # returns a list of objects    
-    for result in results:
-        print(result.media_url)
-        print(result.url)
+    gifs = g.search(search_term) # returns a list of objects    
+    return gifs
+    # for gif in gifs:
+    #     print(gif.media_url)
+    #     print(gif.url)
 
 
 #creates the index page
 @app.route('/')
 def index():
     name = request.values.get('name', "Friends")
-    greeting = "Hello, {}!".format(name)
+    greeting = "Hello, {}!".format(name) 
     return render_template('index.html', greeting=greeting)
 
 
@@ -26,16 +27,17 @@ def index():
 @app.route('/results')
 def results():
     gif = request.values.get('gif')
-    gifs = ["One", "Two", "Three"]
-    return render_template('results.html', gifs=gifs)
-
-
+    results = get_gif(gif)
+    return render_template('results.html', results=results)
+    
 
 #creates the about page
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
 
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
